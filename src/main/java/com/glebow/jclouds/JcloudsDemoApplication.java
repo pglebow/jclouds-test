d949c2ec0743d69f647379c87ae69b1baff5eef4
Philip Glebow <philip_glebow@gap.com>
2016-04-27 10:46:14 -0700

    Setup

---
+++ src/main/java/com/glebow/jclouds/JcloudsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.glebow.jclouds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcloudsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcloudsDemoApplication.class, args);
+	}
+}
